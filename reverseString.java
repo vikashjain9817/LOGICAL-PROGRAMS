026bec0927c1a0a4d8f90bade0e6a761ce2d8e4f
vikashjain9817 <35285405+vikashjain9817@users.noreply.github.com>
2019-01-22 23:29:40 +0530

    Update reverseString.java

--- reverseString.java
+++ reverseString.java
@@ -1,51 +1,33 @@
 import java.util.Scanner;
 import java.util.Arrays;
 public class ReverseString {
-   
-	public static void spaceIndex(String s)
-	{
-		int [] index = new int[10000];
-		for(int k=0;k<index.length;k++)
-			index[k] = 0;
-		int j=0;
-		char [] ch = s.toCharArray();
-		for(int i = 0; i<ch.length; i++)
-		{
-			if(ch[i]== ' ')
-				index[j++] = i;
-		}
-	}
 	public static void reverseString(String s)
 	{
-		int k=0,l=0;
-		char[] rev = new char[100000];
-		String [] s1 = s.split(" ");
-		for(int i=s1.length-1;i>=0;i--)
+		char [] s1 = s.toCharArray();
+		char [] result = new  char[s1.length];
+		for(int i=0;i<s1.length;i++)
 		{
-			char [] a = s1[i].toCharArray();
-			for(int j=a.length-1;j>=0;j--)
-			{
-				 rev[k++]=a[j];
-			}
+			if(s1[i]==' ')
+				result[i] = ' ';
 		}
-		for(int i = 0; i<rev.length; i++)
-		{
-			if(i==index[l])
-			{
-				System.out.println(' ');
-				l++;
-			}
-			else
-			{
-				System.out.println(rev[i]);
-			}
-		}
+		int j = result.length-1;
+		for (int i = 0; i < s1.length; i++)
+		{   
+            if (s1[i] != ' ') {   
+                if (result[j] == ' ') {  
+                    j--;  
+                }     
+                result[j] = s1[i];  
+                j--;  
+            }  
+        }  
+		for(int i = 0;i<result.length;i++)
+			System.out.print(result[i]);
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter string");
 		String s = sc.nextLine();
-		spaceIndex(s);
 		reverseString(s);
 
 	}

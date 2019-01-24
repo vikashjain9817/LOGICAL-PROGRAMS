243c3c16dd23bb184a6ddb532571ca6a5199fec1
vikashjain9817 <35285405+vikashjain9817@users.noreply.github.com>
2019-01-24 09:39:01 +0530

    Create MissingChar.java

---
+++ MissingChar.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class MissingCharcter {
+
+	private static void missingChar(String s1, String s2) {
+		int flag = 0;
+		char[] ch1 = s1.toLowerCase().toCharArray();
+		char[] ch2 = s2.toLowerCase().toCharArray();
+		for(int  i = 0; i < ch2.length; i++)
+		{
+			flag = 0;
+			for(int j = 0; j < ch1.length; j++)
+			{
+				if(ch2[i] == ch1[j])
+				{
+					flag = 1;
+				}
+			}
+			if(flag == 0)
+			{
+				System.out.print(ch2[i]+ " ");
+			}
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter string");
+		String s1 = sc.nextLine();
+		String s2 = "abcdefghijklmnopqrstuvwxyz";
+		missingChar(s1, s2);
+	}
+
+}
